caaa083acbc683155cf2d95138ebef28127ffb09
Esther2504 <esther252000@gmail.com>
2023-02-28 19:07:56 +0100

    start code

--- wordle/src/App.js
+++ wordle/src/App.js
@@ -1,7 +1,54 @@
+import { useState } from "react";
+
 function App() {
+const [guess, setGuess] = useState()
+const [answer, setAnswer] = useState()
+
   return (
     <div className="App">
-      
+      <div style={{display:"flex"}}>
+        <div>g</div>
+        <div>u</div>
+        <div>e</div>
+        <div>s</div>
+        <div>s</div>
+      </div>
+      <div style={{display:"flex"}}>
+        <div>g</div>
+        <div>u</div>
+        <div>e</div>
+        <div>s</div>
+        <div>s</div>
+      </div>
+      <div style={{display:"flex"}}>
+        <div>g</div>
+        <div>u</div>
+        <div>e</div>
+        <div>s</div>
+        <div>s</div>
+      </div>
+      <div style={{display:"flex"}}>
+        <div>g</div>
+        <div>u</div>
+        <div>e</div>
+        <div>s</div>
+        <div>s</div>
+      </div>
+      <div style={{display:"flex"}}>
+        <div>g</div>
+        <div>u</div>
+        <div>e</div>
+        <div>s</div>
+        <div>s</div>
+      </div>
+      <div style={{display:"flex"}}>
+        <div>g</div>
+        <div>u</div>
+        <div>e</div>
+        <div>s</div>
+        <div>s</div>
+      </div>
+      <input type="text" maxLength="5" onChange={e => setGuess(e.target.value)} />
     </div>
   );
 }
