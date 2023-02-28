16986661feccfcef6d2ca3793db1dae162c1b1d4
Esther2504 <esther252000@gmail.com>
2023-02-28 19:46:44 +0100

    basis

--- wordle/src/App.js
+++ wordle/src/App.js
@@ -2,16 +2,26 @@ import { useState } from "react";
 
 function App() {
 const [guess, setGuess] = useState()
-const [answer, setAnswer] = useState()
+const [answer, setAnswer] = useState('tests')
+
+console.log(`answer: ${answer}`)
+console.log(`guess: ${guess}`)
+
+// bij enter guess leegmaken naar volgende regel
+
+const handleSubmit = () => {
+  console.log('test')
+  setGuess()
+}
 
   return (
     <div className="App">
       <div style={{display:"flex"}}>
-        <div>g</div>
-        <div>u</div>
-        <div>e</div>
-        <div>s</div>
-        <div>s</div>
+        <div className="character" style={{width:"50px",height:"50px",border:"3px solid green", fontSize:"1rem"}}>{guess?.charAt(0)}</div>
+        <div className="character">{guess?.charAt(1)}</div>
+        <div className="character">{guess?.charAt(2)}</div>
+        <div className="character">{guess?.charAt(3)}</div>
+        <div className="character">{guess?.charAt(4)}</div>
       </div>
       <div style={{display:"flex"}}>
         <div>g</div>
@@ -48,7 +58,10 @@ const [answer, setAnswer] = useState()
         <div>s</div>
         <div>s</div>
       </div>
-      <input type="text" maxLength="5" onChange={e => setGuess(e.target.value)} />
+      <form>
+      <input type="text" minLength="5" maxLength="5" onChange={e => setGuess(e.target.value)} />
+      <button onClick={e => handleSubmit()}>Submit</button>
+      </form>
     </div>
   );
 }
