392808884d4bf4a5ebc8a4d9bd1a54d906d1b1f1
Esther2504 <esther252000@gmail.com>
2023-03-01 12:40:24 +0100

    css file & guess states

--- wordle/src/App.js
+++ wordle/src/App.js
@@ -1,66 +1,78 @@
 import { useState } from "react";
+import './App.css'
 
 function App() {
-const [guess, setGuess] = useState()
+const [guess1, setGuess1] = useState()
+const [isGuess1, setIsGuess1] = useState(false)
+const [guess2, setGuess2] = useState()
+const [guess3, setGuess3] = useState()
+const [guess4, setGuess4] = useState()
+const [guess5, setGuess5] = useState()
 const [answer, setAnswer] = useState('tests')
 
 console.log(`answer: ${answer}`)
-console.log(`guess: ${guess}`)
+console.log(`guess1: ${guess1}`)
+
+console.log(isGuess1)
 
 // bij enter guess leegmaken naar volgende regel
 
-const handleSubmit = () => {
-  console.log('test')
-  setGuess()
+const handleSubmit = (e) => {
+  e.preventDefault()
+  console.log(`guess1 length: ${guess1.length}`)
+// if guess1 is filled with 5, set true
+if (guess1.length = '5') {
+  setIsGuess1(true)
+}
+
+  // if guess equals answer, become green
+  // if theres no content, stay white. if there is, check if its equal to 
 }
 
   return (
     <div className="App">
-      <div style={{display:"flex"}}>
-        <div className="character" style={{width:"50px",height:"50px",border:"3px solid green", fontSize:"1rem"}}>{guess?.charAt(0)}</div>
-        <div className="character">{guess?.charAt(1)}</div>
-        <div className="character">{guess?.charAt(2)}</div>
-        <div className="character">{guess?.charAt(3)}</div>
-        <div className="character">{guess?.charAt(4)}</div>
+      <div className="guess">
+        <p className="character">{guess1?.charAt(0)}</p>
+        <p className="character">{guess1?.charAt(1)}</p>
+        <p className="character">{guess1?.charAt(2)}</p>
+        <p className="character">{guess1?.charAt(3)}</p>
+        <p className="character">{guess1?.charAt(4)}</p>
       </div>
-      <div style={{display:"flex"}}>
-        <div>g</div>
-        <div>u</div>
-        <div>e</div>
-        <div>s</div>
-        <div>s</div>
+      <div className="guess">
+        <p className="character">{guess2?.charAt(0)}</p>
+        <p className="character">{guess2?.charAt(1)}</p>
+        <p className="character">{guess2?.charAt(2)}</p>
+        <p className="character">{guess2?.charAt(3)}</p>
+        <p className="character">{guess2?.charAt(4)}</p>
       </div>
-      <div style={{display:"flex"}}>
-        <div>g</div>
-        <div>u</div>
-        <div>e</div>
-        <div>s</div>
-        <div>s</div>
+      <div className="guess">
+        <p className="character">{guess3?.charAt(0)}</p>
+        <p className="character">{guess3?.charAt(1)}</p>
+        <p className="character">{guess3?.charAt(2)}</p>
+        <p className="character">{guess3?.charAt(3)}</p>
+        <p className="character">{guess3?.charAt(4)}</p>
       </div>
-      <div style={{display:"flex"}}>
-        <div>g</div>
-        <div>u</div>
-        <div>e</div>
-        <div>s</div>
-        <div>s</div>
+      <div className="guess">
+        <p className="character">{guess4?.charAt(0)}</p>
+        <p className="character">{guess4?.charAt(1)}</p>
+        <p className="character">{guess4?.charAt(2)}</p>
+        <p className="character">{guess4?.charAt(3)}</p>
+        <p className="character">{guess4?.charAt(4)}</p>
       </div>
-      <div style={{display:"flex"}}>
-        <div>g</div>
-        <div>u</div>
-        <div>e</div>
-        <div>s</div>
-        <div>s</div>
-      </div>
-      <div style={{display:"flex"}}>
-        <div>g</div>
-        <div>u</div>
-        <div>e</div>
-        <div>s</div>
-        <div>s</div>
+      <div className="guess">
+        <p className="character">{guess5?.charAt(0)}</p>
+        <p className="character">{guess5?.charAt(1)}</p>
+        <p className="character">{guess5?.charAt(2)}</p>
+        <p className="character">{guess5?.charAt(3)}</p>
+        <p className="character">{guess5?.charAt(4)}</p>
       </div>
       <form>
-      <input type="text" minLength="5" maxLength="5" onChange={e => setGuess(e.target.value)} />
-      <button onClick={e => handleSubmit()}>Submit</button>
+      <input type="text" minLength="5" maxLength="5" required onChange={e => setGuess1(e.target.value)} />
+      <input type="text" minLength="5" maxLength="5" required onChange={e => setGuess2(e.target.value)} />
+      <input type="text" minLength="5" maxLength="5" required onChange={e => setGuess3(e.target.value)} />
+      <input type="text" minLength="5" maxLength="5" required onChange={e => setGuess4(e.target.value)} />
+      <input type="text" minLength="5" maxLength="5" required onChange={e => setGuess5(e.target.value)} />
+      <button type="submit" onClick={e => handleSubmit(e)}>Submit</button>
       </form>
     </div>
   );
