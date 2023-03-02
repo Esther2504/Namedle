4ce53820431d0399390b6b66e131db2ae2df25bf
Esther2504 <esther252000@gmail.com>
2023-03-02 20:09:08 +0100

    plan

--- wordle/src/App.js
+++ wordle/src/App.js
@@ -1,55 +1,62 @@
 import { useState } from "react";
-import './App.css'
+import "./App.css";
 
 function App() {
-const [guesses, setGuesses] = useState([])
-const [guess, setGuess] = useState()
-const [answer, setAnswer] = useState('kinds')
-const [color, setColor] = useState('')
+  const [guesses, setGuesses] = useState([]);
+  const [guess, setGuess] = useState();
+  const [answer, setAnswer] = useState("kinds");
+  const [color, setColor] = useState("");
+  const [guessesLeft, setguessesLeft] = useState()
 
-console.log(`answer: ${answer}`)
-console.log(`guess1: ${guess}`)
+  console.log(`answer: ${answer}`);
+  console.log(`guess1: ${guess}`);
 
-console.log(guesses[0])
+  console.log(guesses[0]);
 
-// bij enter guess leegmaken naar volgende regel
+  // bij enter guess leegmaken naar volgende regel
 
-const handleSubmit = (e) => {
-  e.preventDefault();
-  // console.log(`guess1 length: ${guesses[0].length}`)
-// if guess1 is filled with 5, set true
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log(`guess1 length: ${guesses[0].length}`)
+    // if guess1 is filled with 5, set true
 
-setGuesses([...guesses, guess])
+    setGuesses([...guesses, guess]);
 
-document.getElementById('textbar').value = '';
+    // Naast guess ook een kleur meegeven 
+    // {
+    //   guess:
+    //   kleuren:
+    // }
 
-setGuess()
+    // if guess[0] is answer[0] = groen
+    // if answer includes guess[0] = orange
 
-  // if guess equals answer, become green
-  // if theres no content, stay white. if there is, check if its equal to 
+    document.getElementById("textbar").value = "";
 
-  // map guesses on top, remove bottom guess
+    setGuess();
 
-  if (guesses.length = 1) {
-  // remove 
-  console.log('hiii')
+    // if guess equals answer, become green
+    // if theres no content, stay white. if there is, check if its equal to
 
-}
-}
+    // map guesses on top, remove bottom guess
 
-
+    if ((guesses.length = 1)) {
+      // remove
+      console.log("hiii");
+    }
+  };
 
   return (
     <div className="App">
-{guesses.map(guess =>
-  <div className="guess">
-  <p className="character">{guess?.charAt(0)}</p>
-  <p className="character">{guess?.charAt(1)}</p>
-  <p className="character">{guess?.charAt(2)}</p>
-  <p className="character">{guess?.charAt(3)}</p>
-  <p className="character">{guess?.charAt(4)}</p>
-</div>
-)}
+      {guesses.map((guess) => (
+        <div className="guess">
+          <p className="character">{guess?.charAt(0)}</p>
+          <p className="character">{guess?.charAt(1)}</p>
+          <p className="character">{guess?.charAt(2)}</p>
+          <p className="character">{guess?.charAt(3)}</p>
+          <p className="character">{guess?.charAt(4)}</p>
+        </div>
+      ))}
 
       <div className="guess">
         <p className="character">{guess?.charAt(0)}</p>
@@ -59,8 +66,15 @@ setGuess()
         <p className="character">{guess?.charAt(4)}</p>
       </div>
       <form onSubmit={handleSubmit}>
-      <input type="text" id="textbar" minLength="5" maxLength="5" required onChange={e => setGuess(e.target.value)} />
-      <button type="submit">Submit</button>
+        <input
+          type="text"
+          id="textbar"
+          minLength="5"
+          maxLength="5"
+          required
+          onChange={(e) => setGuess(e.target.value)}
+        />
+        <button type="submit">Enter</button>
       </form>
     </div>
   );
