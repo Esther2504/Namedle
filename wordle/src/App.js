eccda91e3de17e233aef7c62c727ee461a19e1f5
Esther2504 <esther252000@gmail.com>
2023-03-04 13:28:00 +0100

    new colors, winning message

--- wordle/src/App.js
+++ wordle/src/App.js
@@ -5,28 +5,15 @@ function App() {
   const [guesses, setGuesses] = useState([]);
   const [guess, setGuess] = useState();
   const [answer, setAnswer] = useState("kinds");
-  // const [colors, setColors] = useState([]);
   const [guessesLeft, setguessesLeft] = useState(6)
-
-  console.log(`answer: ${answer}`);
-  console.log(`guess: ${guess}`);
-
-  console.log(guesses[0]);
-
-  // console.log(colors)
-
-  // bij enter guess leegmaken naar volgende regel
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // console.log(`guess1 length: ${guesses[0].length}`)
-    // if guess1 is filled with 5, set true
 
     if (guessesLeft > 0) {
    
       console.log(guess.charAt(0))
       console.log(answer.charAt(0))
-      // aan array toevoegen
 
       console.log(Array.from(guess))
       setGuess(Array.from(guess))
@@ -37,14 +24,14 @@ let colors = []
 for (let i = 0; i < 5; i++) {
   if (guess.charAt(i) == answer.charAt(i)) {
     console.log('test')
-    colors.push('green')
+    colors.push('#9DCD8C')
     console.log(colors)
   } else if (answer.includes(guess.charAt(i))) {
     console.log('test2')
-    colors.push('yellow')
+    colors.push('#FEE4A7')
   } else {
     console.log('test3')
-    colors.push('#B2B2B2')
+    colors.push('#FED2BF')
   }
 }
 
@@ -55,23 +42,19 @@ for (let i = 0; i < 5; i++) {
 
     document.getElementById("textbar").value = "";
 
+    if (guess == answer){
+alert('you won!')
+  };
+
     setGuess();
 
     setguessesLeft(guessesLeft - 1)
 
- 
-
-    // if guess equals answer, become green
-    // if theres no content, stay white. if there is, check if its equal to
-
-    // map guesses on top, remove bottom guess
-
-
+  }
 
   }
-  };
 
-  console.log(guesses)
+
 
   return (
     <div className="App">
