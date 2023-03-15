fd58fc12f91e952f5ab225e99c7faf1723af86ed
Esther2504 <esther252000@gmail.com>
2023-03-15 16:02:36 +0100

    namedle logo

--- wordle/src/ExplantionModal.jsx
+++ wordle/src/ExplantionModal.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 export default function ExplantionModal() {
   return (
-    <div>ExplantionModal
-
-      geinspireerd bij wordle
+    <div className="wrapper">
+    <div className="result-container">
+      <p className="cross" onClick={(e) => setOpenModal(false)}>X</p>
+      <div className="content">
+        </div>
     </div>
+  </div>
   )
 }
